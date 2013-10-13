4bc11c609b5b0b0a358ce7000f5e5827510a6cb9
panagiotis <panagiotis@panagiotis-home>
2013-10-13 20:24:44 +0300

    Init commit

--- filecomparator/src/filecomparator/Filecomparator.java
+++ filecomparator/src/filecomparator/Filecomparator.java
@@ -4,6 +4,8 @@
  */
 package filecomparator;
 
+import filecomparator.init.Launcher;
+
 /**
  *
  * @author panagiotis
@@ -14,6 +16,6 @@ public class Filecomparator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Launcher.initProgram(args);
     }
 }
